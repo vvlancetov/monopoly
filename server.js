a9e4ab45a6a2cb740b41964a7cb78e2588585377
Vitalii <30289970+vvlancetov@users.noreply.github.com>
2018-09-15 00:34:53 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+// server.js
+// Для начала установим зависимости.
+const path = "root/var/www/html";
+const http = require('http');
+const routing = require(path + '/routing');
+
+
+let server = new http.Server(function(req, res) {
+  // API сервера будет принимать только POST-запросы и только JSON, так что записываем
+  // всю нашу полученную информацию в переменную jsonString
+  var jsonString = '';
+  res.setHeader('Content-Type', 'application/json');
+  req.on('data', (data) => { // Пришла информация - записали.
+      jsonString += data;
+  });
+
+  req.on('end', () => {// Информации больше нет - передаём её дальше.
+      routing.define(req, res, jsonString); // Функцию define мы ещё не создали.
+  });
+});
+server.listen(80, 'localhost');
